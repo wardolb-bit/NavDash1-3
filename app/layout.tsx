ddcf8cc27c03f3a8da4864468039be4ceed0bdb9
wardolb-bit <wardolb@live.com>
2026-09-03 00:00:08 -1100

    Use robust AIS target capture on main chart

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import { BridgeConsoleRouteGate } from '../components/BridgeConsoleRouteGate';
 import { WxRoutingBridgeSkin } from '../components/WxRoutingBridgeSkin';
 import { CelestialConsoleSkin } from '../components/CelestialConsoleSkin';
 import { CelestialSunMoon } from '../components/CelestialSunMoon';
-import { AisTargetMapBridge } from '../components/AisTargetMapBridge';
+import { AisTargetMapBridgeV2 } from '../components/AisTargetMapBridge.v2';
 
 export const metadata: Metadata = {
   title: 'M/V MB480 NavDash 1.3',
@@ -147,7 +147,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           `}
         </Script>
 
-        <AisTargetMapBridge />
+        <AisTargetMapBridgeV2 />
         <NavDashNav />
         {children}
         <BridgeConsoleRouteGate />
