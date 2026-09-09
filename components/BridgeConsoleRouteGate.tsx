d7337813f3bcc37cd421577d3efa07e81a729309
wardolb-bit <wardolb@live.com>
2026-09-09 12:29:39 -1100

    Mount NOAA ENC detail tuner

--- components/BridgeConsoleRouteGate.tsx
+++ components/BridgeConsoleRouteGate.tsx
@@ -13,6 +13,7 @@ import { BridgeRouteDistanceWgs84 } from "./BridgeRouteDistanceWgs84";
 import { BridgeLegSequenceDisplay } from "./BridgeLegSequenceDisplay";
 import { NavMapMainOverlayV2 } from "./NavMapMainOverlayV2";
 import { NoaaLeafletPaneWeatherOverlay } from "./NoaaLeafletPaneWeatherOverlay";
+import { NoaaEncDetailTuner } from "./NoaaEncDetailTuner";
 import { CelestialConsoleSkin } from "./CelestialConsoleSkin";
 import { MsiConsoleSkin } from "./MsiConsoleSkin";
 import { NavDashMainLinkGuard } from "./NavDashMainLinkGuard";
@@ -86,6 +87,7 @@ export function BridgeConsoleRouteGate() {
       <BridgeConsolePreview />
       <BridgeMapWakeup />
       <NavMapMainOverlayV2 />
+      <NoaaEncDetailTuner />
       <NoaaLeafletPaneWeatherOverlay />
       <BridgeMapLayerControls />
       <BridgeOwnShipEnhancer />
