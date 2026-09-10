2aa765b68b5251a7805f41efafb3b7a493bf2e2e
wardolb-bit <wardolb@live.com>
2026-09-10 01:55:08 -1100

    Mount Main button on tools page

--- app/tools/layout.tsx
+++ app/tools/layout.tsx
@@ -1,5 +1,11 @@
 import "./tools-current.css";
+import ToolsMainButton from "./ToolsMainButton";
 
 export default function ToolsLayout({ children }: { children: React.ReactNode }) {
-  return <div className="navdash-tools-current">{children}</div>;
+  return (
+    <div className="navdash-tools-current">
+      {children}
+      <ToolsMainButton />
+    </div>
+  );
 }
