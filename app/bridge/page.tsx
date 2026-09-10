30c2d2d38b236d6422bc777ff141b8b0a64c4582
wardolb-bit <wardolb@live.com>
2026-09-10 11:59:55 -1100

    Add full NavDash bridge route

---
+++ app/bridge/page.tsx
@@ -0,0 +1,11 @@
+import NavDashConsole from "../NavDashConsole";
+import { MainMapAisTargets } from "../../components/MainMapAisTargets";
+
+export default function BridgePage() {
+  return (
+    <>
+      <NavDashConsole />
+      <MainMapAisTargets />
+    </>
+  );
+}
