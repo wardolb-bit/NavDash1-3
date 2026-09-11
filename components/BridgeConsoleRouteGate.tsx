caec84873f6091b3cc964bb3f42f5e9fa927ef60
wardolb-bit <wardolb@live.com>
2026-09-11 02:24:44 -1100

    Remove Official Weather route skin mount

--- components/BridgeConsoleRouteGate.tsx
+++ components/BridgeConsoleRouteGate.tsx
@@ -13,7 +13,6 @@ import { NavMapZoomLimit } from "./NavMapZoomLimit";
 import { NoaaLeafletPaneWeatherOverlay } from "./NoaaLeafletPaneWeatherOverlay";
 import { CelestialConsoleSkin } from "./CelestialConsoleSkin";
 import { MsiConsoleSkin } from "./MsiConsoleSkin";
-import { OfficialWeatherConsoleSkin } from "./OfficialWeatherConsoleSkin";
 import { NavDashMainLinkGuard } from "./NavDashMainLinkGuard";
 import { SharedRouteSync } from "./SharedRouteSync";
 
@@ -35,13 +34,6 @@ export function BridgeConsoleRouteGate() {
       <>
         <NavDashMainLinkGuard />
         <MsiConsoleSkin />
-      </>
-    );
-  } else if (pathname === "/official-weather") {
-    routeUi = (
-      <>
-        <NavDashMainLinkGuard />
-        <OfficialWeatherConsoleSkin />
       </>
     );
   } else if (isMainNavDashRoute) {
