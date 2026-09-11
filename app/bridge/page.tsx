5d021f1267fdd4a9a71294521454fdfc7962a559
wardolb-bit <wardolb@live.com>
2026-09-10 23:53:41 -1100

    Route narrow bridge screens to mobile console

--- app/bridge/page.tsx
+++ app/bridge/page.tsx
@@ -1,9 +1,11 @@
 import NavDashConsole from "../NavDashConsole";
 import { MainMapAisTargets } from "../../components/MainMapAisTargets";
+import { MobileBridgeRedirect } from "../../components/MobileBridgeRedirect";
 
 export default function BridgePage() {
   return (
     <>
+      <MobileBridgeRedirect />
       <NavDashConsole />
       <MainMapAisTargets />
     </>
