7d9e88ffe952e41bd39d1ecda0f05aac9c57a57e
wardolb-bit <wardolb@live.com>
2026-09-11 02:24:56 -1100

    Remove Official Weather quick-access link

--- components/BridgeQuickAccess.tsx
+++ components/BridgeQuickAccess.tsx
@@ -62,7 +62,6 @@ export function BridgeQuickAccess() {
       <Link href="/tides" className={buttonClass}>Tides</Link>
       <Link href="/position-report" className={buttonClass}>Position Report</Link>
       <Link href="/nav-brief" className={buttonClass}>Nav Brief</Link>
-      <Link href="/official-weather" className={buttonClass}>Official WX</Link>
       <Link href="/msi" className={buttonClass}>MSI</Link>
       <Link href="/tools" className={buttonClass}>Tools</Link>
     </>,
