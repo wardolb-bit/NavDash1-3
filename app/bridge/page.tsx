9b9f2664c61e72b474c8564f64649c26691d7433
wardolb-bit <wardolb@live.com>
2026-09-12 12:58:21 -1100

    Mount ENC object info on bridge map

--- app/bridge/page.tsx
+++ app/bridge/page.tsx
@@ -2,6 +2,7 @@ import NavDashConsole from "../NavDashConsole";
 import { MainMapAisTargets } from "../../components/MainMapAisTargets";
 import { MobileBridgeRedirect } from "../../components/MobileBridgeRedirect";
 import { AisMapReadyRetry } from "../../components/AisMapReadyRetry";
+import { EncObjectInfo } from "../../components/EncObjectInfo";
 
 export default function BridgePage() {
   return (
@@ -10,6 +11,7 @@ export default function BridgePage() {
       <NavDashConsole />
       <MainMapAisTargets />
       <AisMapReadyRetry />
+      <EncObjectInfo />
     </>
   );
 }
