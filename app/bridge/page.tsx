0e37ff6f2386ec871ff153d7cfc5230e45a2d387
wardolb-bit <wardolb@live.com>
2026-09-12 23:50:04 -1100

    Add Arrival Planner release

--- app/bridge/page.tsx
+++ app/bridge/page.tsx
@@ -4,12 +4,14 @@ import { MobileBridgeRedirect } from "../../components/MobileBridgeRedirect";
 import { AisMapReadyRetry } from "../../components/AisMapReadyRetry";
 import { EncObjectInfo } from "../../components/EncObjectInfo";
 import { EncPopupCloseFix } from "../../components/EncPopupCloseFix";
+import { ArrivalPlannerBridgeButton } from "../../components/ArrivalPlannerBridgeButton";
 
 export default function BridgePage() {
   return (
     <>
       <MobileBridgeRedirect />
       <NavDashConsole />
+      <ArrivalPlannerBridgeButton />
       <MainMapAisTargets />
       <AisMapReadyRetry />
       <EncObjectInfo />
