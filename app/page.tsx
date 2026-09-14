472baa559dfeb2b85f5be731b983c8c71c3811cf
wardolb-bit <wardolb@live.com>
2026-09-14 02:00:30 -1100

    Crew: use active route-leg weather on root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,11 @@
-export { default } from "./phone/page";
+import CrewRouteWeatherBridge from "./CrewRouteWeatherBridge";
+import CrewViewPage from "./phone/page";
+
+export default function CrewRootPage() {
+  return (
+    <>
+      <CrewRouteWeatherBridge />
+      <CrewViewPage />
+    </>
+  );
+}
