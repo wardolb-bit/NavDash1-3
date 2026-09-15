22db849c1673bf2166ac11319f844657265af7b7
wardolb-bit <wardolb@live.com>
2026-09-15 08:23:20 -1100

    Localize celestial console helpers

--- app/celestial/layout.tsx
+++ app/celestial/layout.tsx
@@ -1,11 +1,17 @@
 import type { ReactNode } from "react";
 import MoonPhaseDock from "../../components/MoonPhaseDock";
 import CelestialConstellationEnhancer from "../../components/CelestialConstellationEnhancer";
+import { CelestialConsoleSkin } from "../../components/CelestialConsoleSkin";
+import { CelestialSunMoon } from "../../components/CelestialSunMoon";
 
-export default function CelestialLayout({children}:{children:ReactNode}){
-  return <>
-    {children}
-    <MoonPhaseDock />
-    <CelestialConstellationEnhancer />
-  </>;
+export default function CelestialLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      {children}
+      <CelestialConsoleSkin />
+      <CelestialSunMoon />
+      <MoonPhaseDock />
+      <CelestialConstellationEnhancer />
+    </>
+  );
 }
