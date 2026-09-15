128be18fbb0aaef8d6902ef7188b4b3c6424c314
wardolb-bit <wardolb@live.com>
2026-09-15 02:57:53 -1100

    Enable build type and lint validation

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   experimental: {
     cpus: 1,
     workerThreads: true,
